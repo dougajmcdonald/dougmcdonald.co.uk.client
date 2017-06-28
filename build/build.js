1cf1b2701169341fc05138e2f8f18fba14fe24f0
dougajmcdonald <dougajmcdonald@gmail.com>
2017-06-28 20:09:19 +0100

    remove chalk entiirely

--- build/build.js
+++ build/build.js
@@ -5,7 +5,6 @@ process.env.NODE_ENV = 'production'
 var ora = require('ora')
 var rm = require('rimraf')
 var path = require('path')
-var chalk = require('chalk')
 var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
@@ -26,10 +25,10 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       chunkModules: false
     }) + '\n\n')
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
+    console.log('  Build complete.\n')
+    console.log(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
-    ))
+    )
   })
 })
