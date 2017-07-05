1748818ac0d6df4f1213eed64b2e4824b6aa0e70
dougajmcdonald <dougajmcdonald@gmail.com>
2017-07-05 19:53:04 +0100

    put --production back

--- deploy.cmd
+++ deploy.cmd
@@ -88,14 +88,16 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-echo Building...
+echo Installing...
 
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
+  call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
+
+echo Building...
 
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
@@ -116,7 +118,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
+  call :ExecuteCmd !NPM_CMD! install --production
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
