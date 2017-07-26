c9c20e602469deb47d858980f5a232ab370687f1
dougajmcdonald <dougajmcdonald@gmail.com>
2017-07-26 21:33:44 +0100

    small tweaks

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,9 +1,22 @@
 <template>
   <section class="content">
-    <h2>Hi there!</h2>
-    <p>I'm Doug and this is my personal website with thoughts and experiences on code and shiz. </p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere lorem et commodo sollicitudin. Donec viverra mattis dolor. Fusce eu tincidunt ex. Mauris id orci volutpat, ultrices ligula porttitor, hendrerit odio. </p>
-    <p>Sed quis ligula porta, posuere nibh et, lobortis leo. Curabitur eros justo, molestie id sapien eu, molestie tempor sem. Vestibulum pretium laoreet scelerisque. Praesent eget purus facilisis, posuere mauris vitae, bibendum mauris. Sed rhoncus, elit in tempor pulvinar.</p>
+    <article>
+      <h2>Post title 1</h2>
+      <p>I'm Doug and this is my personal website with thoughts and experiences on code and shiz. </p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere lorem et commodo sollicitudin. Donec viverra mattis dolor. Fusce eu tincidunt ex. Mauris id orci volutpat, ultrices ligula porttitor, hendrerit odio. </p>
+      <p>Sed quis ligula porta, posuere nibh et, lobortis leo. Curabitur eros justo, molestie id sapien eu, molestie tempor sem. Vestibulum pretium laoreet scelerisque. Praesent eget purus facilisis, posuere mauris vitae, bibendum mauris. Sed rhoncus, elit in tempor pulvinar.</p>
+    </article>
+    <article>
+      <h2>Post title 2</h2>
+      <p>Sed quis ligula porta, posuere nibh et, lobortis leo. Curabitur eros justo, molestie id sapien eu, molestie tempor sem. Vestibulum pretium laoreet scelerisque. Praesent eget purus facilisis, posuere mauris vitae, bibendum mauris. Sed rhoncus, elit in tempor pulvinar.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere lorem et commodo sollicitudin. Donec viverra mattis dolor. Fusce eu tincidunt ex. Mauris id orci volutpat, ultrices ligula porttitor, hendrerit odio. </p>
+      <p>Sed quis ligula porta, posuere nibh et, lobortis leo. Curabitur eros justo, molestie id sapien eu, molestie tempor sem. Vestibulum pretium laoreet scelerisque. Praesent eget purus facilisis, posuere mauris vitae, bibendum mauris. Sed rhoncus, elit in tempor pulvinar.</p>
+      <p>Sed quis ligula porta, posuere nibh et, lobortis leo. Curabitur eros justo, molestie id sapien eu, molestie tempor sem. Vestibulum pretium laoreet scelerisque. Praesent eget purus facilisis, posuere mauris vitae, bibendum mauris. Sed rhoncus, elit in tempor pulvinar.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent posuere lorem et commodo sollicitudin. Donec viverra mattis dolor. Fusce eu tincidunt ex. Mauris id orci volutpat, ultrices ligula porttitor, hendrerit odio. </p>
+    </article>
+    <footer>
+      Copyright &copy; 2017
+    </footer>
   </section>
 </template>
 
@@ -33,9 +46,22 @@ export default {
       padding-left: 3em;
     }
 
+    h2 {
+      font-size: 2em;
+      color: $red;
+      margin-bottom: 0.2em;
+      font-weight: 700;
+    }
+
     header {
       margin: 80% 2em 0;
       text-align: right;
+    }
+
+    footer {
+      padding: 1em;
+      color: $grey-blue;
+      text-align: center;
     }
   }
 
